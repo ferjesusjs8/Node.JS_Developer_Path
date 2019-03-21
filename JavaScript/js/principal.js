e5e5526c911a51393e960f73aaf77f476f725b32
fernando.js8 <fernando.js8@outlook.com>
2019-03-21 08:47:37 -0300

    implementado if para peso e altura

--- JavaScript/js/principal.js
+++ JavaScript/js/principal.js
@@ -6,10 +6,30 @@ var primeiroPaciente = document.querySelector("#primeiroPaciente");
 var peso = primeiroPaciente.querySelector(".info-peso").textContent;
 var altura = primeiroPaciente.querySelector(".info-altura").textContent;
 var imc = primeiroPaciente.querySelector(".info-imc");
-var calculoIMC = peso / (altura * altura);
+var pesoValido = true;
+var alturaValida = true;
+var msg = "";
 
-imc.textContent = calculoIMC;
+
+if (peso <= 0 || peso >= 1000)
+{
+    console.log("Peso inválido!");
+    pesoValido = false;
+    msg += "Peso não é valido! ";
+}
+
+if (altura <= 0 || altura >= 4)
+{
+    console.log("Altura inválido!");
+    alturaValida = false;
+    msg += "Altura não é valida! ";
+}
+
+if (pesoValido && alturaValida)
+    msg += peso / (altura * altura);
+
+imc.textContent = msg;
 
 console.log(peso);
 console.log(altura);
-console.log(calculoIMC);
+console.log(msg);
