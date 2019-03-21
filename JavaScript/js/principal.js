822d7b64b2096d166d74a59f15f6c5f2b325e5b4
fernando.js8 <fernando.js8@outlook.com>
2019-03-21 14:22:06 -0300

    Criado laço para buscar todos os pacientes

--- JavaScript/js/principal.js
+++ JavaScript/js/principal.js
@@ -2,34 +2,38 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-var primeiroPaciente = document.querySelector("#primeiroPaciente");
-var peso = primeiroPaciente.querySelector(".info-peso").textContent;
-var altura = primeiroPaciente.querySelector(".info-altura").textContent;
-var imc = primeiroPaciente.querySelector(".info-imc");
-var pesoValido = true;
-var alturaValida = true;
-var msg = "";
+var pacientes = document.querySelectorAll(".paciente");
 
-
-if (peso <= 0 || peso >= 1000)
+for (var i = 0; i < pacientes.length; i++)
 {
-    console.log("Peso inválido!");
-    pesoValido = false;
-    msg += "Peso não é valido! ";
+    var paciente = pacientes[i];
+
+    var peso = paciente.querySelector(".info-peso").textContent;
+    var altura = paciente.querySelector(".info-altura").textContent;
+    var imc = paciente.querySelector(".info-imc");
+    var pesoValido = true;
+    var alturaValida = true;
+    var msg = "";
+
+
+    if (peso <= 0 || peso >= 1000){
+        console.log("Peso inválido!");
+        pesoValido = false;
+        msg += "Peso não é valido! ";
+    }
+
+    if (altura <= 0 || altura >= 4){
+        console.log("Altura inválido!");
+        alturaValida = false;
+        msg += "Altura não é valida! ";
+    }
+
+    if (pesoValido && alturaValida)
+        msg += (peso / (altura * altura)).toFixed(2);
+
+    imc.textContent = msg;
+
+    console.log(peso);
+    console.log(altura);
+    console.log(msg);
 }
-
-if (altura <= 0 || altura >= 4)
-{
-    console.log("Altura inválido!");
-    alturaValida = false;
-    msg += "Altura não é valida! ";
-}
-
-if (pesoValido && alturaValida)
-    msg += peso / (altura * altura);
-
-imc.textContent = msg;
-
-console.log(peso);
-console.log(altura);
-console.log(msg);
