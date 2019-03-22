29b0090ac3431f88970f12b54a2666fedfbfb5e5
fernando.js8 <fernando.js8@outlook.com>
2019-03-21 23:18:36 -0300

    Criado funcao mudar cores em caso de erros

--- JavaScript/js/principal.js
+++ JavaScript/js/principal.js
@@ -8,28 +8,32 @@ for (var i = 0; i < pacientes.length; i++)
 {
     var paciente = pacientes[i];
 
-    var peso = paciente.querySelector(".info-peso").textContent;
-    var altura = paciente.querySelector(".info-altura").textContent;
+    var peso = paciente.querySelector(".info-peso");
+    var altura = paciente.querySelector(".info-altura");
     var imc = paciente.querySelector(".info-imc");
     var pesoValido = true;
     var alturaValida = true;
     var msg = "";
 
 
-    if (peso <= 0 || peso >= 1000){
+    if (peso.textContent <= 0 || peso.textContent >= 1000){
         console.log("Peso inválido!");
         pesoValido = false;
         msg += "Peso não é valido! ";
+        paciente.classList.add("pacienteInvalido");
+        peso.classList.add("atributoComErro");
     }
 
-    if (altura <= 0 || altura >= 4){
+    if (altura.textContent <= 0 || altura.textContent >= 4){
         console.log("Altura inválido!");
         alturaValida = false;
         msg += "Altura não é valida! ";
+        paciente.classList.add("pacienteInvalido");
+        altura.classList.add("atributoComErro");
     }
 
     if (pesoValido && alturaValida)
-        msg += (peso / (altura * altura)).toFixed(2);
+        msg += (peso.textContent / (altura.textContent * altura.textContent)).toFixed(2);
 
     imc.textContent = msg;
 
