7d6e8430044363ad80aefd4ab1d57a2b3c4f5bd1
fernando.js8 <fernando.js8@outlook.com>
2019-03-23 14:38:15 -0300

    Dividindo código em mais funções

--- JavaScript/js/FormAdicionaPaciente.js
+++ JavaScript/js/FormAdicionaPaciente.js
@@ -1,57 +1,64 @@
-var botao = document.querySelector('#adicionar_paciente');
 
-botao.addEventListener("click", function(event){
+var botao = document.querySelector( '#adicionar_paciente' );
 
-    event.preventDefault();
+botao.addEventListener( "click", function( event ){
 
-    var tablePacientes = document.querySelector("#tabela_pacientes");
+    event.preventDefault( );
 
-    var formAdicionaPaciente = document.querySelector("#formAdiciona");
+    var tablePacientes = document.querySelector( "#tabela_pacientes" );
+    var formAdicionaPaciente = document.querySelector( "#formAdiciona" );
+    var erro = document.querySelector( ".messageError" );
+    var paciente = ExtrairDadosFormulario( formAdicionaPaciente );
 
-    var erro = document.querySelector(".messageError");
-
-    var trNovoPaciente = document.createElement("tr");
-
-    var tdNome = document.createElement("td");
-    var tdPeso = document.createElement("td");
-    var tdAltura = document.createElement("td");
-    var tdIMC = document.createElement("td");
-
-    var nome = formAdicionaPaciente.nome.value;
-    var peso = formAdicionaPaciente.peso.value;
-    var altura = formAdicionaPaciente.altura.value;
-    var imc = CalculaIMC(peso, altura);
-
-    
-
-    if ( VerificarPesoAltura(nome, peso, altura) ) {
-        tdNome.textContent = nome;
-        tdPeso.textContent = peso;
-        tdAltura.textContent = altura;
-        tdIMC.textContent = imc;
-
-        trNovoPaciente.appendChild(tdNome);
-        trNovoPaciente.appendChild(tdPeso);
-        trNovoPaciente.appendChild(tdAltura);
-        trNovoPaciente.appendChild(tdIMC);
-    
-        tablePacientes.appendChild(trNovoPaciente);
+    if ( VerificarPesoAltura( paciente.nome, paciente.peso, paciente.altura ) ) {
+        var trNovoPaciente = montaTr( paciente );
+        tablePacientes.appendChild( trNovoPaciente );
         erro.textContent = "";
-
-        formAdicionaPaciente.nome.value = "";
-        formAdicionaPaciente.peso.value = "";
-        formAdicionaPaciente.altura.value = "";
     }
 
     else {
         erro.textContent = "Erro: Dados Inválidos! Verifique os valores digitados!";
     }
+
+    formAdicionaPaciente.reset( );
 });
 
-function VerificarPesoAltura(nome, peso, altura){
+function VerificarPesoAltura( nome, peso, altura ){
     return ( ( nome && peso && altura ) 
         && ( peso > 0 && peso < 500 && altura > 0 && altura < 4 )
-        && ( !isNaN(peso) && !isNaN(altura) )
-        && ( isNaN(nome) ) 
+        && ( !isNaN( peso ) && !isNaN( altura ) )
+        && ( isNaN( nome ) ) 
         );
+}
+
+function ExtrairDadosFormulario( form ){
+
+    var Paciente = {
+        nome: form.nome.value,
+        peso: form.peso.value,
+        altura: form.altura.value,
+        imc: CalculaIMC( form.peso.value, form.altura.value )
+    };
+
+    return Paciente;
+}
+
+function montaTr( paciente ){
+    var tr = document.createElement( "tr" );
+    tr.classList.add( "paciente" );
+
+    tr.appendChild( montaTd( paciente.nome, "info-nome" ) );
+    tr.appendChild( montaTd( paciente.peso, "info-peso" ) );
+    tr.appendChild( montaTd( paciente.altura, "info-altura" ) );
+    tr.appendChild( montaTd( paciente.imc, "info-IMC" ) );
+
+    return tr;
+}
+
+function montaTd( dado, classe ){
+    var td = document.createElement( "td" );
+    td.classList.add( classe );
+    td.textContent = dado;
+    
+    return td;
 }
